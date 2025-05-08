41e476306b2890f51d591118cdc22d45898fd56e
Juan Parrado <jyunyauya@gmail.com>
2025-05-08 12:13:36 -0300

    Falta el ejercicio final.

--- BaseDeDatosChinook.sql
+++ BaseDeDatosChinook.sql
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 --                                                                  --
 --                           EJERCICIO 1                            --
 --                                                                  -- 
@@ -26,6 +18,14 @@ FROM genres g
 INNER JOIN tracks t ON g.GenreId = t.GenreId
 GROUP BY g.name
 ORDER BY "Cantidad de Canciones por Género" DESC;
+--                                                                  --
+--                           EJERCICIO 4                            --
+--                                                                  --
+SELECT a.Title AS "Nombre del Disco", COUNT(t.TrackId) AS "Cantidad de Canciones"
+FROM albums a
+INNER JOIN tracks t ON a.AlbumId = t.AlbumId
+GROUP BY a.Title
+HAVING COUNT(t.TrackId) > 5;
 --                                                                  --
 --                           EJERCICIO 5                            --
 --                                                                  --
@@ -52,5 +52,35 @@ INNER JOIN tracks t ON t.AlbumId = a.AlbumId
 INNER JOIN genres g ON t.GenreId = g.GenreId
 INNER JOIN artists art ON a.ArtistId = art.ArtistId
 ORDER BY t.Milliseconds DESC;
+--                                                                  --
+--                           EJERCICIO 8                            --
+--                                                                  --
+-- Apellido, puesto, apellido del jefe y cantidad de clientes que atiende de todos los empleados, 
+-- ordenado desde los que atienden más clientes a los que atienden menos
+SELECT e.LastName AS "Apellido", e.Title AS "Puesto"
+FROM employees e
+INNER JOIN customers c ON e.LastName = c.LastName
+--                                                                  --
+--                           EJERCICIO 9                            --
+--                                                                  --
+INSERT INTO tracks (Name, MediaTypeId, Composer, Milliseconds, Bytes, UnitPrice) 
+VALUES 
+('Mozart Is A Liar 1', 2, 'Galo Perez', 1776, 2493695, 1.90),
+('Mozart Is A Liar 2', 2, 'Galo Perez', 2376, 1498695, 1.70),
+('Mozart Is A Liar 3', 2, 'Galo Perez', 43976, 3495695, 1.20),
+('Mozart Is A Liar 4', 2, 'Galo Perez', 53176, 8499695, 1.50);
+--                                                                  --
+--                           EJERCICIO 10                           --
+--                                                                  --
+SELECT Name AS "Canción", MediaTypeId, Composer, Milliseconds, Bytes, UnitPrice
+FROM tracks
+WHERE Composer = 'Galo Perez';
+--                                                                  --
+--                           EJERCICIO 11                           --
+--                                                                  --
+UPDATE tracks SET Name = 'Mazorto' WHERE Name = 'Mozart Is A Liar 1';
+UPDATE tracks SET Name = 'Mazierto' WHERE Name = 'Mozart Is A Liar 2';
 
---Me gustan los hombres
+SELECT Name AS "Canción", MediaTypeId, Composer, Milliseconds, Bytes, UnitPrice
+FROM tracks
+WHERE Composer = 'Galo Perez';
